cd0c69d00ee8d6509cc18dfea4a5f3b7df94a8f7
Grant Humphries <humphriesg@hidefsurveying.co.uk>
2019-12-09 16:28:30 +0100

    fix 5432 error

--- biodiversity/settings/base.py
+++ biodiversity/settings/base.py
@@ -172,17 +172,17 @@ DATABASES = {
         }
     }
 
-    import dj_database_url
+import dj_database_url
     
-    #db_from_env = dj_database_url.config(conn_max_age=500)
-    #DATABASES['default'].update(db_from_env)
-    DATABASES['default'] = dj_database_url.config()
-    DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+#db_from_env = dj_database_url.config(conn_max_age=500)
+#DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config()
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
-    SECURE_PROXY_SSL_HEADER = (
-    "HTTP_X_FORWARDED_PROTO", 
-    "https"
-    )
+SECURE_PROXY_SSL_HEADER = (
+"HTTP_X_FORWARDED_PROTO", 
+"https"
+)
 
 
 # Password validation
