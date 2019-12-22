3c661468791e90a718fe1a9aaeffc65e43955494
grwhumphries <humphries.grant@gmail.com>
2019-12-22 16:42:13 +0000

    fixing auth

--- accounts/views.py
+++ accounts/views.py
@@ -45,6 +45,7 @@ class CustomLoginView(LoginView):
         user = authenticate(request=request, username=username, password=password)
         print('LOGGED IN', user)
         login(self.request, user)
+        return self.get_success_url()
         form = self.get_form()
         return self.form_valid(form)
 
