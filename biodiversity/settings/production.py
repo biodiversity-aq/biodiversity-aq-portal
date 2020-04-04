c1d398fb42db20a55feb7510bad77d32c0cd5009
Grant Humphries <grwhumphries@blackbawks.net>
2020-04-05 00:18:29 +0100

    move database creds for production to secrets

--- biodiversity/settings/production.py
+++ biodiversity/settings/production.py
@@ -11,17 +11,7 @@ DEBUG = False
 
 BASE_URL = 'https://www.biodiversity.aq'
 
-DATABASES = {
-            'default': {
-                'ENGINE': #'django.contrib.gis.db.backends.postgis',
-                'NAME': #'d86hngmf7e303f',
-                'USER': #'hwyoarfegnhrwn',
-                'PASSWORD': #'286e142ad8f2d8aa4d5ad529c43bbe847f4037ddc4f065f51bf18366cfac4cd5',
-                'HOST': #'ec2-54-217-234-157.eu-west-1.compute.amazonaws.com',
-                'PORT': #'5432'
-            }
-        }
-
+DATABASES = BIODIVERSITY.DATABASE
 
     
 #db_from_env = dj_database_url.config(conn_max_age=500)
