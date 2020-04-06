710e8728bf8d43fd56b4a14b36824cb9919ab8db
Grant Humphries <grwhumphries@blackbawks.net>
2020-04-06 11:07:39 +0100

    back to heroku db settings

--- biodiversity/wsgi.py
+++ biodiversity/wsgi.py
@@ -5,8 +5,7 @@ WSGI config for biodiversity project.
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "biodiversity.settings.base")
 
-
-from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 application = Cling(get_wsgi_application())
 
