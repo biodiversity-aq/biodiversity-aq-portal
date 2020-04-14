306a659fc8c76afebcb471537d5c8202711547ea
Yi Ming Gan <ymgan@naturalsciences.be>
2020-04-14 12:07:04 +0200

    the hook leads to exceptions for LinkPage is a slug is provided

--- home/wagtail_hooks.py
+++ home/wagtail_hooks.py
@@ -1,22 +1,6 @@
 import wagtail.admin.rich_text.editors.draftail.features as draftail_features
 from wagtail.admin.rich_text.converters.html_to_contentstate import InlineStyleElementHandler, BlockElementHandler
-from wagtail.contrib.redirects.models import Redirect
 from wagtail.core import hooks
-
-
-@hooks.register('before_edit_page')
-def create_redirect_on_slug_change(request, page):
-    """
-    Create redirect when editing slugs
-    https://docs.wagtail.io/en/v2.7/reference/pages/model_recipes.html
-    """
-    if request.method == 'POST':
-        if page.slug != request.POST['slug']:
-            Redirect.objects.create(
-                    old_path=page.url[:-1],
-                    site=page.get_site(),
-                    redirect_page=page
-                )
 
 
 # ----------------------------------
