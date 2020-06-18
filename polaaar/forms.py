8ed31ef3497821ef69444a87f0ff4187a7d19450
Yi Ming Gan <ymgan@naturalsciences.be>
2020-06-18 12:21:45 +0200

    add file extension validation

--- polaaar/forms.py
+++ polaaar/forms.py
@@ -1,12 +1,18 @@
 from .models import MailFile
 from django import forms
+from django.core.validators import FileExtensionValidator
 
 
 class EmailForm(forms.ModelForm):
-    email = forms.EmailField(max_length=200,
+    email = forms.EmailField(max_length=200, label='Your email',
                              widget=forms.TextInput(attrs={'class': "form-control", 'id': "clientemail"}))
-    message = forms.CharField(widget=forms.Textarea(attrs={'class': "form-control"}))
-    subject = forms.CharField(widget=forms.TextInput(attrs={'class': "form-control"}))
+    message = forms.CharField(label='Additional information',
+                              widget=forms.Textarea(attrs={'class': "form-control"}))
+    subject = forms.CharField(label='Dataset name',
+                              widget=forms.TextInput(attrs={'class': "form-control"}))
+    document = forms.FileField(
+        required=True, widget=forms.ClearableFileInput, label='Select data file',
+        validators=[FileExtensionValidator(allowed_extensions=['txt', 'tsv', 'csv', 'xls', 'xlsx', 'zip'])])
 
     class Meta:
         model = MailFile
