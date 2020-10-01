088805992304e0d8d02cf7ea8d23443bc6a5da01
Yi Ming Gan <ymgan@naturalsciences.be>
2020-10-01 12:34:55 +0200

    add static url

--- polaaar/urls.py
+++ polaaar/urls.py
@@ -1,4 +1,7 @@
 from __future__ import unicode_literals
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from django.conf.urls import include, url
 from . import views
@@ -56,3 +59,5 @@ urlpatterns = [
     path('api-auth/',include('rest_framework.urls',namespace='rest_framework'))
  
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
