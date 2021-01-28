be6ae4f6dcf7627f362bf30f176e767741bb58f4
Yi Ming Gan <ymgan@naturalsciences.be>
2021-01-28 13:03:17 +0100

    increase page size

--- biodiversity/settings/base.py
+++ biodiversity/settings/base.py
@@ -379,7 +379,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.NamespaceVersioning',
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 20
 }
 
 # POLAAAR SETTINGS
